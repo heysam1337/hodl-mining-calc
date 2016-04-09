f642668bd6997bd8eaeda548d06b1501d05ed852
Sam Jones <sam.jones@gmail.com>
2016-04-08 23:52:24 -0400

    edits in temp app data

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-var tempNetHashRate = 171419;
-var tempReward = 28050;
+// replace these with current numbers 
+var tempNetHashRate = 171419; 
+var tempReward = 28050; // daily coins generated on the network
+
+var netHashRate = tempNetHashRate;
+var reward = tempNetward;
+
+// earnings per day
+function calcReward(){
+	$("#earnings").text($("userHash").value * reward / netHashRate);
+}
+
+$("#userHash").change(alert("changed"));
+//$("#userHash").change(calcReward());
+
+alert("changed");
