5dc5d2e3a79375da322d17c5c97ad6e13d78612e
Sam Jones <sam.jones@gmail.com>
2016-04-10 00:49:58 -0400

    Time based values

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ var exchangeRate = tempExchangeRate;
 var reward = tempReward;
 
 $("#userHash").keyup(function () {
-	var dailyValue = ($("#userHash").val() * reward * exchangeRate) / netHashRate
-	$("#earnings").text("$" + dailyValue.toFixed(2));
+	var dailyValue = ($("#userHash").val() * reward * exchangeRate) / netHashRate;
+	var weeklyValue = dailyValue * 7;
+	var monthlyValue = dailyValue * 30;
+	$("#dailyEarnings").text("Earnings per day: $" + dailyValue.toFixed(2));
+	$("#weeklyEarnings").text("Earnings per week: $" + weeklyValue.toFixed(2));
+	$("#monthlyEarnings").text("Earnings per month: $" + monthlyValue.toFixed(2));
 });
 
 
