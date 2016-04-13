47c90c1d9e2921f2c21b369761d7e8ae68df77b5
Sam Jones <sam.jones@gmail.com>
2016-04-13 02:12:29 -0400

    added data feed

---
+++ save-coin-data.php
@@ -0,0 +1,30 @@
+<?php
+
+	$ch1 = curl_init();
+	curl_setopt_array($ch1, array(
+    	CURLOPT_RETURNTRANSFER => 1,
+    	CURLOPT_URL => 'http://yobit.net/api/3/ticker/hodl_btc',
+    	CURLOPT_USERAGENT => 'HodlCoin Miner'
+	));
+
+	$resp1 = json_decode(curl_exec($ch1));
+	
+	curl_close($ch1);
+
+	$ch2 = curl_init();
+	curl_setopt_array($ch2, array(
+    	CURLOPT_RETURNTRANSFER => 1,
+    	CURLOPT_URL => 'http://yobit.net/api/3/ticker/btc_usd',
+    	CURLOPT_USERAGENT => 'HodlCoin Miner'
+	));
+	
+	$resp2 = json_decode(curl_exec($ch2));
+
+	curl_close($ch2);
+
+	$exchangeRate = $resp1->hodl_btc->last * $resp2->btc_usd->last;
+
+	$file = 'hodldata.js';
+	$data = 'var hodlData = {"netHashRate":126700,"exchangeRate":'. $exchangeRate . ',"reward":28050};';
+	file_put_contents($file, $data, LOCK_EX);
+?>
