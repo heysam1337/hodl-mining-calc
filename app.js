1b9d31d0c5d96604ed176e20fd974cc8623fe9a5
Sam Jones <sam.jones@gmail.com>
2016-04-25 16:10:39 -0400

    variables for bonus

--- app.js
+++ app.js
@@ -11,9 +11,24 @@ function updateReward() {
 		$("#monthlyEarnings").text("Earnings per month: " + monthlyValue.toFixed(2) + " Hodl ($" + (monthlyValue * exchangeRate).toFixed(2) +")");
 }
 
-function calcInterest() {
+function updateScenario() {
 
 	// get term deposits formula for interest and scenario section. 
+
+
+	var principal;
+	var term;
+	var standardInterest; // (1/2^22)
+
+	// bonus interest
+	// Principal + (Standard Interest + (Bonus Interest * Bonus Multiplier))
+	var bonusInterest;
+	var bonusMultipler;
+
+
+	// Full bonus on term is Principal + ((Standard Interest + (Bonus Interest * Bonus Multiplier))*Term Deposit Multiplier)
+	var termDepositMultipler; // (1-((409530-X)/409530)^6)*100  X is number of blocks to lock for, min 2 days, max 1 year
+
 
 	var result;
 
