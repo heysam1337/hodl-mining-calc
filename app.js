4db28435e553de46a66510e7e1ef65eed3d93f99
Sam Jones <sam.jones@gmail.com>
2016-04-26 16:12:15 -0400

    Added network and exchange info and refactored

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 var netHashRate = hodlData.netHashRate;
-var exchangeRate = hodlData.exchangeRate;
 var reward = hodlData.reward;
+var currentBlock = hodlData.lastBlock;
+var exchangeHODLBTC = hodlData.exchangeHODLBTC;
+var exchangeBTCUSD = hodlData.exchangeBTCUSD;
+var exchangeHODLUSD =  exchangeHODLBTC * exchangeBTCUSD ;
 
 function updateReward() {
 		var dailyValue = ($("#userHash").val() * reward) / netHashRate;
 		var weeklyValue = dailyValue * 7;
 		var monthlyValue = dailyValue * 30;
-		$("#dailyEarnings").text("Earnings per day: " + dailyValue.toFixed(2) + " Hodl ($" + (dailyValue * exchangeRate).toFixed(2) +")");
-		$("#weeklyEarnings").text("Earnings per week: " + weeklyValue.toFixed(2) + " Hodl ($" + (weeklyValue * exchangeRate).toFixed(2) +")");
-		$("#monthlyEarnings").text("Earnings per month: " + monthlyValue.toFixed(2) + " Hodl ($" + (monthlyValue * exchangeRate).toFixed(2) +")");
+		$("#dailyEarnings").text("Earnings per day: " + dailyValue.toFixed(2) + " Hodl ($" + (dailyValue * exchangeHODLUSD).toFixed(2) +")");
+		$("#weeklyEarnings").text("Earnings per week: " + weeklyValue.toFixed(2) + " Hodl ($" + (weeklyValue * exchangeHODLUSD).toFixed(2) +")");
+		$("#monthlyEarnings").text("Earnings per month: " + monthlyValue.toFixed(2) + " Hodl ($" + (monthlyValue * exchangeHODLUSD).toFixed(2) +")");
 }
 
 function updateScenario() {
 
 	// get term deposits formula for interest and scenario section. 
-
+	// https://bitcointalk.org/index.php?topic=1317918.0
+    var blockTime = 154; // in seconds
+    var blocksPerDay = (24*60*60) / 154; // 561.038961
 
 	var principal;
 	var term;
@@ -22,7 +27,7 @@ function updateScenario() {
 
 	// bonus interest
 	// Principal + (Standard Interest + (Bonus Interest * Bonus Multiplier))
-	var bonusInterest;
+	var bonusInterest; // Compounded, the rate is 2174%. It's reduced every block by a multiplier - calculated like this =((409530-X)/409530)^4 (X is the block where the balance is recorded as an output).
 	var bonusMultipler;
 
 
@@ -40,8 +45,19 @@ $("#miningHardware").change(function () {
 	$("#userHash").val($("#miningHardware").val());
 	updateReward();
 });
-$("#exchangeRate").text("Current Exchange Rate: $" + exchangeRate.toFixed(5));
-$("#netHashRate").text("Current Network Hashrate: " + netHashRate + " H/s");
+
+var netInfoString = "<b>Exchange Rate:</b> $" + exchangeHODLUSD.toFixed(5);
+netInfoString = netInfoString + "<br /><b>HODL to BTC:</b> " + exchangeHODLBTC.toFixed(8);
+netInfoString = netInfoString + "<br /><b>BTC to USD:</b> $" + exchangeBTCUSD.toFixed(2);
+netInfoString = netInfoString + "<br /><br /><b>Current Block:</b> " + currentBlock;
+netInfoString = netInfoString + "<br /><b>Network Hashrate:</b> " + netHashRate;
+
+
+$("#networkInfo").html(netInfoString);
+
+
+$("#exchangeRate").html("Exchange Rate: $" + exchangeHODLUSD.toFixed(5) + "<br /> (HODL to BTC: " + exchangeHODLBTC.toFixed(8) + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; BTC to USD:" + exchangeBTCUSD.toFixed(2) +")");
+
 
 
 
--- save-coin-data.php
+++ save-coin-data.php
@@ -27,7 +27,7 @@
 	$ch3 = curl_init();
 	curl_setopt_array($ch3, array(
     	CURLOPT_RETURNTRANSFER => 1,
-    	CURLOPT_URL => 'http://www.tekyexplorer.xyz/hodl/api_fetch.php?method=getnetworkhashps',
+    	CURLOPT_URL => 'http://www.tekyexplorer.xyz/hodl/api_fetch.php?method=getmininginfo',
     	CURLOPT_USERAGENT => 'HodlCoin Miner'
 	));
 	
@@ -35,11 +35,17 @@
 
 	curl_close($ch3);
 
-	$netHashRate = $resp3->data;
-	$exchangeRate = $resp1->hodl_btc->last * $resp2->btc_usd->last;
+	$exchangeHODLBTC = $resp1->hodl_btc->last;
+	$exchangeBTCUSD = $resp2->btc_usd->last;
+
+	$netHashRate = $resp3->data->networkhashps;
+	$lastBlock = $resp3->data->blocks;
+
+
+	//$exchangeRate = $exchangeHODLBTC * $exchangeBTCUSD;
 
 
 	$file = dirname(__FILE__) . '/hodldata.js';
-	$data = 'var hodlData = {"netHashRate":'. $netHashRate . ',"exchangeRate":'. $exchangeRate . ',"reward":28050,"updated":' . time() .'};';
+	$data = 'var hodlData = {"netHashRate":'. $netHashRate . ',"exchangeHODLBTC":' . number_format($exchangeHODLBTC, 8) . ',"exchangeBTCUSD":'. $exchangeBTCUSD . ',"lastBlock":'. $lastBlock . ',"reward":28050,"updated":' . time() .'};';
 	file_put_contents($file, $data, LOCK_EX);
 ?>
